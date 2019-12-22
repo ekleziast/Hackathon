f24a5c2e2089c39fb1907959743034c1dea2f10e
ekleziast <rami1@mail.ru>
2019-12-22 14:00:07 +0500

    Login, Registration

--- Database/Model/Account.cs
+++ Database/Model/Account.cs
@@ -14,10 +14,11 @@ namespace Database.Model
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Key]
         public Guid ID { get; set; }
-
-        public string FIO { get; set; }
+        
         public string Email { get; set; }
         public string Password { get; set; }
-
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
     }
 }
--- OrenburgTourismFM/Controllers/UserController.cs
+++ OrenburgTourismFM/Controllers/UserController.cs
@@ -9,21 +9,81 @@ using System.Web.Http;
 namespace OrenburgTourismFM.Controllers
 {
     public class UserController : ApiController
-    {
-
-        private Context context = new Context();
+    { 
         [HttpPost]
-        public void Registration ([FromBody]Account account)
+        public HttpStatusCode Registration ([FromBody]Account account)
         {
-
+            if (!CheckRegistration(account))
+            {
+                return HttpStatusCode.BadRequest;
+            }
             try
             {
-
-                context.Accounts.Add(account);
-                context.SaveChanges();
+                using (Context db = new Context())
+                {
+                    db.Accounts.Add(account);
+                    db.SaveChanges();
+                }
             }
             catch
             {
+                return HttpStatusCode.BadGateway;
+            }
+            return HttpStatusCode.OK;
+        }
+        private static bool CheckRegistration(Account account)
+        {
+            try
+            {
+                var email = new System.Net.Mail.MailAddress(account.Email);
+                if(email.Address != account.Email)
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
+            if (String.IsNullOrWhiteSpace(account.Password))
+            {
+                return false;
+            }
+            if (String.IsNullOrWhiteSpace(account.FirstName))
+            {
+                return false;
+            }
+            if (String.IsNullOrWhiteSpace(account.MiddleName))
+            {
+                return false;
+            }
+            if (String.IsNullOrWhiteSpace(account.LastName))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        [HttpPost]
+        public Account Login([FromUri]string email, [FromUri]string password)
+        {
+            try
+            {
+                using (Context db = new Context())
+                {
+                    var account = db.Accounts.Where(o => o.Email == email && o.Password == password).FirstOrDefault();
+                    if(account == null)
+                    {
+                        throw new HttpResponseException(HttpStatusCode.Forbidden);
+                    }
+                    return account;
+                }
+            }
+            catch
+            {
+                throw new HttpResponseException(HttpStatusCode.BadGateway);
             }
         }
     }
